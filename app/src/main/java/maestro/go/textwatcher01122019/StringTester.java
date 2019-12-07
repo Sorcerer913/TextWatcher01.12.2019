6ff767c2f741f3e34b73f7412df7c9fa07884574
Sorcerer913 <7964277@gmail.com>
2019-12-08 01:28:00 +0300

    bug fixed: working StringTester

--- app/src/main/java/maestro/go/textwatcher01122019/StringTester.java
+++ app/src/main/java/maestro/go/textwatcher01122019/StringTester.java
@@ -1,29 +1,29 @@
 package maestro.go.textwatcher01122019;
 
 public class StringTester{
-    public boolean contains(String string, String[] array){
+    public boolean contains(String string, String[] array){     //  Returns True if Something in Array includes in String
         for (String str: array) {
-            if(!string.contains(str)){
-                return false;
+            if(string.contains(str)){
+                return true;
             }
         }
-        return true;
+        return false;
     }
     public boolean contains(String string, String[] array, boolean shiftIsMatter){
         if(shiftIsMatter){
             for (String str: array) {
-                if(!string.contains(str)){
-                    return false;
+                if(string.contains(str)){
+                    return true;
                 }
             }
-            return true;
+            return false;
         }else{
             for (String str: array) {
-                if(!string.toLowerCase().contains(str.toLowerCase())){
-                    return false;
+                if(string.toLowerCase().contains(str.toLowerCase())){
+                    return true;
                 }
             }
-            return true;
+            return false;
         }
     }
 
